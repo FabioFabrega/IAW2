5e1d8d27de86c551a28965793b3a62dd25606106
FabioFabrega <fabiofabrega746@gmail.com>
2025-01-27 10:55:00 +0100

    clase 27/01/2025

---
+++ next-asir/app/conectarnest/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+import { useState, useEffect } from 'react'
+export default function Home() {
+ const [message, setMessage] = useState('Cargando…')
+ useEffect(() => {
+ fetch('http://localhost:4000/carrito')
+ .then(response => response.text())
+ .then(data => setMessage(data))
+ .catch(error => setMessage('Error al cargar el mensaje'))
+ }, [])
+return ( <p> Mensaje del servidor: {message} </p> )
+}
